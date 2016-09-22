46d8063ab16ba2ffb6bf23cc61fd610134dbbc5b
Herman verschooten <Herman@verschooten.net>
2016-09-22 21:52:58 +0200

    Remove clutter from mix file

--- mix.exs
+++ mix.exs
@@ -4,10 +4,6 @@ defmodule GpioRpi.Mixfile do
   def project do
     [app: :gpio_rpi,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.2",
       name: "gpio_rpi",
       description: description(),
