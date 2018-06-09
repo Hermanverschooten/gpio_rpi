fcad9bb3f66ffd02ebdc5bfc81f3cdd2ff55d924
Herman verschooten <Herman@verschooten.net>
2018-06-09 18:16:37 +0200

    Bump version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GpioRpi.Mixfile do
 
   def project do
     [app: :gpio_rpi,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.2",
       name: "gpio_rpi",
       description: description(),
